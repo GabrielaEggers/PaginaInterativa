cc478ed71395e15b22530fdad135a4d581a8eee4
GabrielaEggers <167655686+GabrielaEggers@users.noreply.github.com>
2025-07-31 08:11:32 -0300

    .

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container')
+function categoria(categotia, pergunta, resposta) {
+    let container = document.getELementetByI('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
-    cartao.innerHTML = `
-    <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
-    <div class="cartao__conteudo__pergunta">
-        <p>${pergunta}</p>
+    cartao.innerHTML =
+    <div clas="cartao_conteuso">
+        <h3>${categoria}</h3>
+        <div class="cartao_conteudo_pergunta">
+            <p>${pergunta}</p>
     </div>
-    <div class="cartao__conteudo__resposta">
+    <div class="cartao_conteu0_respota">
         <p>${resposta}</p>
     </div>
     </div>
-    `
 
     let respostaEstaVisivel = false
 
     function viraCartao() {
         respostaEstaVisivel = !respostaEstaVisivel
-        cartao.classList.toggle('active', respostaEstaVisivel)
+        cartao.classList.toggle('active' respostaEstaVisivel)
     }
     cartao.addEventListener('click', viraCartao)
 
 
-    container.appendChild(cartao)
-
-}
+    container.appenChil(cartao)
