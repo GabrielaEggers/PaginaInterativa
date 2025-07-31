244a66db043de4f6f46397b49cbeb61132903716
GabrielaEggers <167655686+GabrielaEggers@users.noreply.github.com>
2025-07-31 07:33:17 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,26 +1,29 @@
-criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-)
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
 
-criaCartao(
-    'Geografia',
-    'Qual a capital da França',
-    'A capital da França é Paris'
+    let respostaEstaVisivel = false
 
-)
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
 
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
 
-)
+    container.appendChild(cartao)
 
-criaCartao(
-    'Lingua inglesa',
-    'Como se iz oi em inglês',
-    'Oi em inglês é HI (RAI)'
-)
+}
